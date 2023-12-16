01213576b2510c8dc03b1666bff736f99ba52a0b
joonas <joonas@Fnetti.local>
2023-12-17 00:52:29 +0200

    dotfyles

--- nvim/lua/config-alpha.lua
+++ nvim/lua/config-alpha.lua
@@ -1,36 +1,58 @@
 local status_ok, alpha = pcall(require, "alpha")
 if not status_ok then
-    return
+	return
 end
 
 local dashboard = require("alpha.themes.dashboard")
 
-local RandomQuotes = {
-    "Panta rhei, kai ouden menei.",
-    "No honor among thieves.",
-}
-
-local randomQuote = RandomQuotes[math.random(#RandomQuotes)]
+function dayLeft()
+	local now = os.date("*t")
+	local totalSeconds = 24 * 60 * 60
+	local secondsPassed = now.hour * 60 * 60 + now.min * 60 + now.sec
+	local dayLeftPercentage = (totalSeconds - secondsPassed) / totalSeconds * 100
+	return dayLeftPercentage
+end
+function monthLeft()
+	local now = os.date("*t")
+	local totalDaysInMonth = os.date("*t", os.time({ year = now.year, month = now.month + 1, day = 0 })).day
+	local monthLeftPercentage = (totalDaysInMonth - now.day) / totalDaysInMonth * 100
+	return monthLeftPercentage
+end
+function yearLeft()
+	local now = os.date("*t")
+	local yearStart = os.time({ year = now.year, month = 1, day = 1 })
+	local yearEnd = os.time({ year = now.year + 1, month = 1, day = 1 })
+	local totalSecondsInYear = yearEnd - yearStart
+	local secondsPassedThisYear = os.time() - yearStart
+	local yearLeftPercentage = (totalSecondsInYear - secondsPassedThisYear) / totalSecondsInYear * 100
+	return yearLeftPercentage
+end
+function lifeLeft()
+	local birthYear = 1994
+	local currentYear = os.date("*t").year
+	local age = currentYear - birthYear
+	local lifeLeftPercentage = (79 - age) / 79 * 100
+	return lifeLeftPercentage
+end
 
 dashboard.section.header.val = {
-    "", randomQuote, "",
-    "",
-    "",
-    "",
+	"DAY Left " .. string.format("%.2f", dayLeft()) .. "%",
+	"MONTH Left " .. string.format("%.2f", monthLeft()) .. "%",
+	"YEAR Left " .. string.format("%.2f", yearLeft()) .. "%",
+	"LIFE Left " .. string.format("%.2f", lifeLeft()) .. "%",
 }
-
 
 dashboard.section.header.opts.hl = "AlphaHeader"
 
 dashboard.section.buttons.val = {
-    dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-    dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-    dashboard.button("r", "  Recent files", ":Telescope oldfiles <CR>"),
-    dashboard.button("p", "  Find text", ":Telescope live_grep <CR>"),
-    dashboard.button("b", "  Bookmarks", ":Telescope marks <CR>"),
-    dashboard.button("c", "  Config", ":e ~/.config/nvim <CR>"),
-    dashboard.button("s", "  Session", ":Telescope session-lens search_session<CR>"),
-    dashboard.button("q", "  Quit", ":qa <CR>"),
+	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+	dashboard.button("r", "  Recent files", ":Telescope oldfiles <CR>"),
+	dashboard.button("p", "  Find text", ":Telescope live_grep <CR>"),
+	dashboard.button("b", "  Bookmarks", ":Telescope marks <CR>"),
+	dashboard.button("c", "  Config", ":e ~/.config/nvim <CR>"),
+	dashboard.button("s", "  Session", ":Telescope session-lens search_session<CR>"),
+	dashboard.button("q", "  Quit", ":qa <CR>"),
 }
 
 dashboard.opts.opts.noautocmd = true
@@ -40,7 +62,7 @@ alpha.setup(dashboard.opts)
 local footer = require("alpha.themes.dashboard")
 
 footer.section.footer.val = {
-    "  " .. os.date(" %H:%M "),
-    "  " .. os.date("%d/%m/%Y"),
+	"  " .. os.date(" %H:%M "),
+	"  " .. os.date("%d/%m/%Y"),
 }
 dashboard.section.footer.opts.hl = "AlphaFooter"
